d9bd7a87d87df35870d6d113613f7f9c0439cbf4
Andrei Pronsky <95336511+AndreiPronsky@users.noreply.github.com>
2025-05-13 14:41:27 +0300

    Initial commit

---
+++ src/test/java/org/pronsky/transfer_service/TestTransferServiceApplication.java
@@ -0,0 +1,11 @@
+package org.pronsky.transfer_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestTransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(TransferServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
