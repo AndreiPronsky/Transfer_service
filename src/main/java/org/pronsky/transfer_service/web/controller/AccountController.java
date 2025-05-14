9fa0feaaac7d6dcf63d32cb80af9ad248a295446
Andrei Pronsky <95336511+AndreiPronsky@users.noreply.github.com>
2025-05-14 14:01:48 +0300

    Controller performed

--- src/main/java/org/pronsky/transfer_service/web/controller/AccountController.java
+++ src/main/java/org/pronsky/transfer_service/web/controller/AccountController.java
@@ -1,5 +1,29 @@
 package org.pronsky.transfer_service.web.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.pronsky.transfer_service.service.AccountService;
+import org.pronsky.transfer_service.service.dto.request.TransferRequestDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/accounts")
+@Validated
+@RequiredArgsConstructor
 public class AccountController {
 
+    private final AccountService accountService;
+
+    @PostMapping("/transfer")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Void> performTransfer(@RequestBody @Valid TransferRequestDto request) {
+        accountService.performTransfer(request);
+        return ResponseEntity.accepted().build();
+    }
 }
