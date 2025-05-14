3a976aaeb2389345c3cc6a61d12a6c65043071ee
Andrei Pronsky <95336511+AndreiPronsky@users.noreply.github.com>
2025-05-14 07:34:57 +0300

    Demo request dto validation performed

--- src/main/java/org/pronsky/transfer_service/service/dto/request/SearchUserRequestDto.java
+++ src/main/java/org/pronsky/transfer_service/service/dto/request/SearchUserRequestDto.java
@@ -1,5 +1,9 @@
 package org.pronsky.transfer_service.service.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,8 +11,20 @@ import lombok.Getter;
 @Builder
 public class SearchUserRequestDto {
 
+    @NotBlank(message = "Date of birth must not be blank")
+    @Pattern(regexp = "\\d{2}\\.\\d{2}\\.\\d{4}", message = "Date of birth must be in the format dd.MM.yyyy")
     private String dateOfBirth;
+
+    @NotBlank(message = "Phone number must not be blank")
+    @Pattern(regexp = "7\\d{10}", message = "Phone number must start with '7' and contain 11 digits total")
     private String phone;
+
+    @NotBlank(message = "Name must not be blank")
+    @Size(max = 500, message = "Name must be less than or equal to 500 characters")
     private String name;
+
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Email must be valid")
+    @Size(max = 200, message = "Email must be less than or equal to 200 characters")
     private String email;
 }
--- src/main/java/org/pronsky/transfer_service/service/dto/request/TransferRequestDto.java
+++ src/main/java/org/pronsky/transfer_service/service/dto/request/TransferRequestDto.java
@@ -1,15 +1,27 @@
 package org.pronsky.transfer_service.service.dto.request;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
+
 @Getter
 @Builder
 public class TransferRequestDto {
 
+    @NotNull(message = "Sender account ID must not be null")
+    @Positive(message = "Sender account ID must be a positive number")
     private Long transferFrom;
+
+    @NotNull(message = "Recipient account ID must not be null")
+    @Positive(message = "Recipient account ID must be a positive number")
     private Long transferTo;
+
+    @NotNull(message = "Transfer amount must not be null")
+    @DecimalMin(value = "0.01", message = "Transfer amount must be at least 0.01")
     private BigDecimal amount;
 }
